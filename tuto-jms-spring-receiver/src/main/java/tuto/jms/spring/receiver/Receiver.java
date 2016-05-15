36917debaa422e5c4359d2b485284d76ac441a08
jhoanseve <jhoanseve6@gmail.com>
2016-05-15 02:50:58 -0500

    JMS Receiver

---
+++ tuto-jms-spring-receiver/src/main/java/tuto/jms/spring/receiver/Receiver.java
@@ -0,0 +1,16 @@
+package tuto.jms.spring.receiver;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.jms.annotation.JmsListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Receiver {
+	private static final Logger LOGGER = LoggerFactory.getLogger(Receiver.class);
+	
+	@JmsListener(destination="mailbox-destination")
+	public void reveiveMessage(String message) {
+		LOGGER.info("reveiveMessage: {}", message);
+	}
+}
