2d48ab2ce67b117b5367f1b8b033edd9e938b8fa
jhoanseve <jhoanseve6@gmail.com>
2016-05-17 17:42:27 -0500

    Sendto

--- tuto-jms-spring-receiver/src/main/java/tuto/jms/spring/receiver/Receiver.java
+++ tuto-jms-spring-receiver/src/main/java/tuto/jms/spring/receiver/Receiver.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.jms.annotation.JmsListener;
+import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Component;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -24,16 +25,22 @@ public class Receiver {
 	}
 	
 	
+	@SendTo("test-dest")
 	@JmsListener(destination = "${activemq.queue.objectMessage}")
-	public void reveiveObjectMessage(String message) {
+	public String reveiveObjectMessage(String message) {
 		PersonaDTO dto;
 		
 		try {
 			dto = objectMapper.readValue(message, PersonaDTO.class);
 			LOGGER.info("reveiveObjectMessage: [{}]", dto);
 			
+			dto.setApellidos("Nuevo Apellido");
+			
+			return objectMapper.writeValueAsString(dto);
 		} catch (IOException e) {
 			LOGGER.error("Error al intentar convertir JSON en PersonaDTO", e);
 		}
+		
+		return null;
 	}
 }
